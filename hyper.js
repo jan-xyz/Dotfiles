4665e8a15518a1933680b55ac814b8bb74010632
Jan Steinke <jan.steinke@gmail.com>
2019-07-06 14:23:05 +0200

    add Font to hyper config

--- hyper.js
+++ hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"Source Code Pro for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
