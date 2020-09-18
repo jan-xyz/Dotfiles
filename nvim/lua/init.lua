2e6e92311d1c13db96ac6accff839c97ad81816e
Jan Steinke <jan.steinke@gmail.com>
2020-09-18 23:42:34 +0200

    use nvim lua plugins for completion and diag

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -11,22 +11,3 @@ nvim_lsp.vimls.setup{}
 nvim_lsp.metals.setup{}
 nvim_lsp.r_language_server.setup{}
 nvim_lsp.sumneko_lua.setup{}
-
--- add Diagnostics to quickfix list
--- https://github.com/neovim/nvim-lsp/issues/69#issuecomment-616816555
-do
-  local method = "textDocument/publishDiagnostics"
-  local default_callback = vim.lsp.callbacks[method]
-  vim.lsp.callbacks[method] = function(err, method, result, client_id)
-    default_callback(err, method, result, client_id)
-    if result and result.diagnostics then
-      for _, v in ipairs(result.diagnostics) do
-        v.bufnr = client_id
-        v.lnum = v.range.start.line + 1
-        v.col = v.range.start.character + 1
-        v.text = v.message
-      end
-      vim.lsp.util.set_qflist(result.diagnostics)
-    end
-  end
-end
