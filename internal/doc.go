0efe581b568be0a177bf2d5fc37e65de5142cddd
Jan Steinke <jan.steinke@gmail.com>
2021-01-03 18:33:22 +0100

    add documentation for dotfiles packages

---
+++ internal/doc.go
@@ -0,0 +1,4 @@
+// dotfiles contains implementations for various tools and preferences
+// which can be configured with the tool. It includes installing and updating
+// packages as well as configurations.
+package dotfiles
