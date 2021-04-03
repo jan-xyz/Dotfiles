5d78e7d0cb2b093dd6ab07b33734b2c1b033763d
Jan Steinke <jan.steinke@gmail.com>
2021-04-03 18:51:25 +0200

    re-write on_attach configuration

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,7 +1,16 @@
 -- languageClients:
 local nvim_lsp = require'lspconfig'
 
-nvim_lsp.bashls.setup{}
+local on_attach = function(client, bufnr)
+  -- Autoformat on save
+  if client.resolved_capabilities.document_formatting then
+    vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 100)]]
+  end
+end
+
+nvim_lsp.bashls.setup{
+  on_attach = on_attach,
+}
 nvim_lsp.dockerls.setup{}
 nvim_lsp.efm.setup{
   init_options = {documentFormatting = true},
@@ -17,9 +26,11 @@ nvim_lsp.efm.setup{
     }
 }
 nvim_lsp.pyls.setup{
+  on_attach = on_attach,
   cmd = {'/usr/local/bin/python3', '-m', 'pyls'};
 }
 nvim_lsp.rls.setup{
+  on_attach = on_attach,
   settings = {
     rust = {
       clippy_preference = "on",
@@ -27,6 +38,7 @@ nvim_lsp.rls.setup{
   }
 }
 nvim_lsp.gopls.setup{
+  on_attach = on_attach,
   settings = {
     gopls = {
       gofumpt = true,
@@ -39,12 +51,14 @@ nvim_lsp.gopls.setup{
   }
 }
 nvim_lsp.kotlin_language_server.setup{
+  on_attach = on_attach,
   cmd = {'kotlin-language-server'};
 }
 nvim_lsp.vimls.setup{}
 nvim_lsp.metals.setup{}
 nvim_lsp.r_language_server.setup{}
 nvim_lsp.sumneko_lua.setup{
+  on_attach = on_attach,
   cmd = {'/Users/jan/dotfiles/submodules/lua-language-server/bin/macOS/lua-language-server', '-E', '/Users/jan/dotfiles/submodules/lua-language-server/main.lua'},
   settings = {
     Lua = {
@@ -92,17 +106,3 @@ tree_sitter.setup {
     enable = true,
   },
 }
-
-local on_attach = function(client, bufnr)
-  -- Autoformat on save
-  if client.resolved_capabilities.document_formatting then
-      vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 100)]]
-  end
-end
-
--- Use a loop to conveniently both setup defined servers
--- and map buffer local keybindings when the language server attaches
-local servers = { "gopls", "rls", "pyls" }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach }
-end
