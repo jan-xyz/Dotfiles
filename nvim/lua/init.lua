4ef57d8def0f48615f24756f2b5d2fdf1b7982cb
Jan Steinke <jan.steinke@gmail.com>
2021-04-03 17:52:58 +0200

    more sumneko config

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -48,9 +48,26 @@ nvim_lsp.sumneko_lua.setup{
   cmd = {'/Users/jan/dotfiles/submodules/lua-language-server/bin/macOS/lua-language-server', '-E', '/Users/jan/dotfiles/submodules/lua-language-server/main.lua'},
   settings = {
     Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
       diagnostics = {
         globals = { 'vim' }
-      }
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
     }
   }
 }
