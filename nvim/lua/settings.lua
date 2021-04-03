413a415c7183730daf7e6ca6de658a595f688b2b
Jan Steinke <jan.steinke@gmail.com>
2021-04-03 23:26:25 +0200

    migrate some settings to lua

---
+++ nvim/lua/settings.lua
@@ -0,0 +1,21 @@
+local o = vim.o
+local wo = vim.wo
+local bo = vim.bo
+
+-- global options
+o.hidden=true         -- Enable hidden buffers
+o.signcolumn='yes'    -- Show signs in number column (e.g. errors and warnings)
+o.list=true           -- Show tabs, spaces and line ends
+o.mouse='a'           -- Enable all mouse support
+
+-- window options
+wo.number=true         -- Add line numbers
+wo.colorcolumn="80"      -- Show a line at the 80th character
+
+-- buffer options
+bo.expandtab=true      -- Expand tabs to spaces per default
+bo.shiftwidth=2        -- Set default auto-indent width
+bo.softtabstop=2       -- Set default virtual tabstop width
+bo.tabstop=2           -- Set default tabstop width
+
+vim.cmd 'colorscheme nord'  -- Set color scheme
