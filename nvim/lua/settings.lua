c69e9a5601988697b9782aa4237134122e508d34
Jan Steinke <jan.steinke@gmail.com>
2021-04-04 14:46:13 +0200

    add documentation

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -7,16 +7,16 @@ o.hidden=true         -- Enable hidden buffers
 o.signcolumn='yes'    -- Show signs in number column (e.g. errors and warnings)
 o.list=true           -- Show tabs, spaces and line ends
 o.mouse='a'           -- Enable all mouse support
-o.termguicolors=true
+o.termguicolors=true  -- enable 24-bit colors
 
 -- window options
-wo.number=true         -- Add line numbers
-wo.colorcolumn="80"      -- Show a line at the 80th character
+wo.number=true      -- Add line numbers
+wo.colorcolumn="80" -- Show a line at the 80th character
 
 -- buffer options
-bo.expandtab=true      -- Expand tabs to spaces per default
-bo.shiftwidth=2        -- Set default auto-indent width
-bo.softtabstop=2       -- Set default virtual tabstop width
-bo.tabstop=2           -- Set default tabstop width
+bo.expandtab=true -- Expand tabs to spaces per default
+bo.shiftwidth=2   -- Set default auto-indent width
+bo.softtabstop=2  -- Set default virtual tabstop width
+bo.tabstop=2      -- Set default tabstop width
 
 vim.cmd 'colorscheme nord'  -- Set color scheme
