cf014fc7ff69d18813853738e96e343a19b2fc0a
Jan Steinke <jan.steinke@gmail.com>
2021-04-04 15:02:17 +0200

    do alias vim packages

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,22 +1,18 @@
-local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
-
 -- global options
-o.hidden=true         -- Enable hidden buffers
-o.signcolumn='yes'    -- Show signs in number column (e.g. errors and warnings)
-o.list=true           -- Show tabs, spaces and line ends
-o.mouse='a'           -- Enable all mouse support
-o.termguicolors=true  -- enable 24-bit colors
+vim.o.hidden=true         -- Enable hidden buffers
+vim.o.signcolumn='yes'    -- Show signs in number column (e.g. errors and warnings)
+vim.o.list=true           -- Show tabs, spaces and line ends
+vim.o.mouse='a'           -- Enable all mouse support
+vim.o.termguicolors=true  -- enable 24-bit colors
 
 -- window options
-wo.number=true      -- Add line numbers
-wo.colorcolumn="80" -- Show a line at the 80th character
+vim.wo.number=true      -- Add line numbers
+vim.wo.colorcolumn="80" -- Show a line at the 80th character
 
 -- buffer options
-bo.expandtab=true -- Expand tabs to spaces per default
-bo.shiftwidth=2   -- Set default auto-indent width
-bo.softtabstop=2  -- Set default virtual tabstop width
-bo.tabstop=2      -- Set default tabstop width
+vim.bo.expandtab=true -- Expand tabs to spaces per default
+vim.bo.shiftwidth=2   -- Set default auto-indent width
+vim.bo.softtabstop=2  -- Set default virtual tabstop width
+vim.bo.tabstop=2      -- Set default tabstop width
 
 vim.cmd 'colorscheme nord'  -- Set color scheme
