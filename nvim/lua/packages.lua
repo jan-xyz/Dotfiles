c7804d3c121163f532d2638f2f949c319a22dc42
Jan Steinke <jan.steinke@gmail.com>
2021-04-17 15:36:09 +0200

    move to toggleterm

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -4,11 +4,10 @@ return require('packer').startup(function(use)
   use { 'arcticicestudio/nord-vim', branch= 'develop' }
   use 'majutsushi/tagbar'
   use 'liuchengxu/vista.vim'
-  use 'voldikss/vim-floaterm'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'kyazdani42/nvim-tree.lua'
+  use {'kyazdani42/nvim-tree.lua', requires ='kyazdani42/nvim-web-devicons'}
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use {'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
+  use {'akinsho/nvim-toggleterm.lua'}
 
   -- Misc
   use { 'junegunn/fzf', run = function() vim.fn['fzf#install()'](0) end }
