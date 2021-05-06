014530a38315efb956f989000da2825c6622172b
Jan Steinke <jan.steinke@gmail.com>
2021-05-06 09:13:56 +0200

    do not close tree on open

--- nvim/lua/file_explorer.lua
+++ nvim/lua/file_explorer.lua
@@ -26,6 +26,6 @@ vim.g.nvim_tree_bindings = {
 
 vim.g.nvim_tree_auto_open = 1
 vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_quit_on_open = 0
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_lsp_diagnostics = 1
