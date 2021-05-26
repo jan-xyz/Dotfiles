7b03aed955dc5c84e2d9d918d01acc5a2b3f576e
Jan Steinke <jan.steinke@gmail.com>
2021-05-26 19:06:17 +0200

    add separator for File Explorer

--- nvim/lua/bufferline_config.lua
+++ nvim/lua/bufferline_config.lua
@@ -1,5 +1,6 @@
 require'bufferline'.setup{
   options = {
-    diagnostics = "nvim_lsp"
+    diagnostics = "nvim_lsp",
+    offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
   }
 }
