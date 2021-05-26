f4508eaf86d0fb3dc523c38551ea1f985f6d86a9
Jan Steinke <jan.steinke@gmail.com>
2021-05-26 19:07:05 +0200

    add lua config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -31,10 +31,13 @@ local on_attach = function(client, bufnr)
   end
 end
 
-nvim_lsp.bashls.setup{
+-- Docker
+nvim_lsp.dockerls.setup{
   on_attach = on_attach,
 }
-nvim_lsp.dockerls.setup{
+
+-- Shell/Bash
+nvim_lsp.bashls.setup{
   on_attach = on_attach,
 }
 nvim_lsp.efm.setup{
@@ -50,10 +53,14 @@ nvim_lsp.efm.setup{
         }
     }
 }
+
+-- Python
 nvim_lsp.pyls.setup{
   on_attach = on_attach,
   cmd = {'/usr/local/bin/python3', '-m', 'pyls'};
 }
+
+-- Rust
 nvim_lsp.rust_analyzer.setup{
   on_attach = on_attach,
   settings = {
@@ -66,6 +73,8 @@ nvim_lsp.rust_analyzer.setup{
         }
     }
 }
+
+-- Go
 nvim_lsp.gopls.setup{
   on_attach = on_attach,
   settings = {
@@ -79,47 +88,43 @@ nvim_lsp.gopls.setup{
     }
   }
 }
+
+-- Kotlin
 nvim_lsp.kotlin_language_server.setup{
   on_attach = on_attach,
   cmd = {'kotlin-language-server'};
 }
+
+-- VIM
 nvim_lsp.vimls.setup{
   on_attach = on_attach,
 }
+
+-- Scala
 nvim_lsp.metals.setup{
   on_attach = on_attach,
 }
+
+-- R
 nvim_lsp.r_language_server.setup{
   on_attach = on_attach,
 }
-nvim_lsp.sumneko_lua.setup{
-  on_attach = on_attach,
-  cmd = {'/Users/jan/dotfiles/submodules/lua-language-server/bin/macOS/lua-language-server', '-E', '/Users/jan/dotfiles/submodules/lua-language-server/main.lua'},
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        globals = { 'vim' }
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-        },
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    }
-  }
-}
+
+-- LUA
+local luadev = require("lua-dev").setup({
+  lspconfig = {
+    on_attach = on_attach,
+    cmd = {
+      '/Users/jan/dotfiles/submodules/lua-language-server/bin/macOS/lua-language-server',
+      '-E',
+      '/Users/jan/dotfiles/submodules/lua-language-server/main.lua',
+    },
+  },
+})
+local lspconfig = require('lspconfig')
+lspconfig.sumneko_lua.setup(luadev)
+
+-- YAML
 nvim_lsp.yamlls.setup{
   on_attach = on_attach,
   settings = {
