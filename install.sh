5d147fb7bef37619ecafac48d633be95d3734afe
Jan Steinke <jan.steinke@gmail.com>
2021-05-26 19:53:35 +0200

    add information for symlinking JDK

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ fi
 make run
 
 # setup system Java wrappers to find this JDK
+echo -e "${YELLOW}Setting up Java wrappers for homebrew JDK${NOCOLOR}"
 sudo ln -sfn "$(brew --prefix)/opt/openjdk@11/libexec/openjdk.jdk" "/Library/Java/JavaVirtualMachines/openjdk-11.jdk"
 sudo ln -sfn "$(brew --prefix)/opt/openjdk@8/libexec/openjdk.jdk" "/Library/Java/JavaVirtualMachines/openjdk-8.jdk"
 
