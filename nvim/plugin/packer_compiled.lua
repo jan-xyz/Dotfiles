e2359f2a8fbffd5462fee2d7bc613ef4bd5836d3
Jan Steinke <jan.steinke@gmail.com>
2021-07-23 21:37:26 +0200

    packer sync

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -69,6 +69,10 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ale = {
+    loaded = true,
+    path = "/Users/jan/.local/share/nvim/site/pack/packer/start/ale"
+  },
   ["completion-nvim"] = {
     loaded = true,
     path = "/Users/jan/.local/share/nvim/site/pack/packer/start/completion-nvim"
@@ -77,10 +81,6 @@ _G.packer_plugins = {
     config = { "\27LJ\2\nD\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\1\nnumhl\2\nsetup\rgitsigns\frequire\0" },
     loaded = true,
     path = "/Users/jan/.local/share/nvim/site/pack/packer/start/gitsigns.nvim"
-  },
-  ["kotlin-vim"] = {
-    loaded = true,
-    path = "/Users/jan/.local/share/nvim/site/pack/packer/start/kotlin-vim"
   },
   ["lsp_extensions.nvim"] = {
     loaded = true,
@@ -157,14 +157,6 @@ _G.packer_plugins = {
   ["vim-floaterm"] = {
     loaded = true,
     path = "/Users/jan/.local/share/nvim/site/pack/packer/start/vim-floaterm"
-  },
-  ["vim-vsnip"] = {
-    loaded = true,
-    path = "/Users/jan/.local/share/nvim/site/pack/packer/start/vim-vsnip"
-  },
-  ["vim-vsnip-integ"] = {
-    loaded = true,
-    path = "/Users/jan/.local/share/nvim/site/pack/packer/start/vim-vsnip-integ"
   },
   ["vista.vim"] = {
     loaded = true,
