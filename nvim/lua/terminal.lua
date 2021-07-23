f03c0a5320f39053682e125f1d2800158e2269bc
Jan Steinke <jan.steinke@gmail.com>
2021-07-23 21:12:22 +0200

    go back to floaterm

--- nvim/lua/terminal.lua
+++ nvim/lua/terminal.lua
@@ -2,14 +2,10 @@ local wk = require("which-key")
 
 -- keymaps in normal mode
 wk.register({
-	t = { "<cmd>ToggleTerm<CR>", "toggle terminal", noremap = true },
+	t = { "<cmd>FloatermToggle<CR>", "toggle terminal", noremap = true },
 }, { prefix = "<leader>" })
 
 -- keymaps in terminal mode
 wk.register({
-	t = { "<cmd>ToggleTerm<CR>", "toggle terminal", noremap = true },
+	t = { "<cmd>FloatermToggle<CR>", "toggle terminal", noremap = true },
 }, { mode = "t", prefix = "<leader>" })
-
-require("toggleterm").setup({
-	direction = "float",
-})
