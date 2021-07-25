6e58b3193456e8f913435dc82d147c82aa26403a
Jan Steinke <jan.steinke@gmail.com>
2021-07-25 13:59:42 +0200

    move dap language settings into language modules

--- nvim/lua/languages/go.lua
+++ nvim/lua/languages/go.lua
@@ -1,7 +1,8 @@
 local nvim_lsp = require("lspconfig")
 local lsp = require("lsp")
+local dap = require("dap")
 
--- Go
+-- LSP config
 nvim_lsp.gopls.setup({
 	on_attach = lsp.on_attach,
 	settings = {
@@ -15,3 +16,40 @@ nvim_lsp.gopls.setup({
 		},
 	},
 })
+
+-- DAP config
+dap.adapters.go = function(callback, config)
+	local handle
+	local pid_or_err
+	local port = 38697
+	handle, pid_or_err = vim.loop.spawn("dlv", {
+		args = { "dap", "-l", "127.0.0.1:" .. port },
+		detached = true,
+	}, function(code)
+		handle:close()
+		print("Delve exited with exit code: " .. code)
+	end)
+	-- Wait 100ms for delve to start
+	vim.defer_fn(function()
+		--dap.repl.open()
+		callback({ type = "server", host = "127.0.0.1", port = port })
+	end, 100)
+
+	--callback({type = "server", host = "127.0.0.1", port = port})
+end
+-- Go default launch configs
+dap.configurations.go = {
+	{
+		type = "go",
+		name = "Debug current file",
+		request = "launch",
+		program = "${file}",
+	},
+	{
+		type = "go",
+		name = "Debug tests in current dir", -- configuration for debugging test files
+		request = "launch",
+		mode = "test",
+		program = "${fileDirname}",
+	},
+}
--- nvim/lua/languages/rust.lua
+++ nvim/lua/languages/rust.lua
@@ -1,6 +1,7 @@
 local lsp = require("lsp")
+local dap = require("dap")
 
--- Rust
+-- LSP config
 vim.cmd(
 	[[autocmd InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *.rs :lua require'lsp_extensions'.inlay_hints{ prefix = ' » ', highlight = "NonText", enabled = {"ChainingHint"} }]]
 )
@@ -18,3 +19,52 @@ require("rust-tools").setup({
 		},
 	},
 })
+
+-- DAP config
+dap.adapters.lldb = {
+	type = "executable",
+	command = "/usr/local/Cellar/llvm/12.0.1/bin/lldb-vscode",
+	name = "lldb",
+}
+dap.configurations.rust = {
+	{
+		name = "Debug binary",
+		type = "lldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+		args = {},
+
+		-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+		--
+		--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+		--
+		-- Otherwise you might get the following error:
+		--
+		--    Error on launch: Failed to attach to the target process
+		--
+		-- But you should be aware of the implications:
+		-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+		runInTerminal = false,
+	},
+	{
+		type = "lldb",
+		request = "launch",
+		name = "Debug selected unit test",
+		cargo = {
+			args = {
+				"test",
+				"--no-run",
+			},
+			filter = {
+				name = "libthat",
+				kind = "lib",
+			},
+		},
+		args = { "${selectedText}" },
+		cwd = "${workspaceFolder}",
+	},
+}
