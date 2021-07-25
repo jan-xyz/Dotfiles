d77e081eb37144fec2a4eb7dd568e9aa562aef41
Jan Steinke <jan.steinke@gmail.com>
2021-07-25 15:02:37 +0200

    add k8s yaml schema

--- nvim/lua/languages/yaml.lua
+++ nvim/lua/languages/yaml.lua
@@ -6,10 +6,13 @@ nvim_lsp.yamlls.setup({
 	on_attach = lsp.on_attach,
 	settings = {
 		yaml = {
+			validate = true,
+			formate = { enabled = true },
 			schemas = {
 				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
 				["http://json.schemastore.org/github-action"] = ".github/**/action.{yml,yaml}",
 				["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+				["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "**/*.k8s.{yml,yaml}",
 			},
 		},
 	},
