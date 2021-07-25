32e948484c8e749fd9377b18ace8100be8359eba
Jan Steinke <jan.steinke@gmail.com>
2021-07-25 13:55:48 +0200

    hide confirm dialog for commits

--- nvim/lua/git.lua
+++ nvim/lua/git.lua
@@ -1,3 +1,11 @@
+local use = require("packer").use
+use({
+	"TimUntersberger/neogit",
+	config = function()
+		require("neogit").setup({ disable_commit_confirmation = true })
+	end,
+})
+
 -- keymaps
 local wk = require("which-key")
 wk.register({
