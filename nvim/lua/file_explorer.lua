70cff15ef4657abe4c2ddf70570f1324a31fb9b7
Jan Steinke <jan.steinke@gmail.com>
2021-08-13 17:17:06 +0200

    don't auto close tree
      This causes an error with closing buffers when the tree is open.

--- nvim/lua/file_explorer.lua
+++ nvim/lua/file_explorer.lua
@@ -7,7 +7,7 @@ wk.register({
 })
 
 vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_auto_close = 0
 vim.g.nvim_tree_quit_on_open = 0
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_lsp_diagnostics = 1
