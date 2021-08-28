e6f8d41521180b760db31a6bbad85972b58b9227
Jan Steinke <jan.steinke@gmail.com>
2021-08-28 17:54:53 +0200

    more modularization clean up

--- nvim/lua/lsp/packages.lua
+++ nvim/lua/lsp/packages.lua
@@ -1,8 +1,18 @@
 local packer = require("packer")
 
+-- Symbols config
+packer.use({
+	"liuchengxu/vista.vim",
+	config = function()
+		vim.g.vista_default_executive = "nvim_lsp"
+	end,
+})
+
+-- config object
+
 packer.use("neovim/nvim-lspconfig")
 packer.use("nvim-lua/lsp_extensions.nvim")
 packer.use("nvim-lua/completion-nvim")
 packer.use("simrat39/rust-tools.nvim") -- LSP server extensions for Rust
-packer.use("folke/lua-dev.nvim")
+packer.use("folke/lua-dev.nvim") -- LSP server extensions for Lua
 packer.use("glepnir/lspsaga.nvim")
