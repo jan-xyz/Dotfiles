09b86d38b86c8900ca00711fbe871c2d6c27321f
Jan Steinke <jan.steinke@gmail.com>
2021-08-28 18:51:43 +0200

    load language config only when lsp loads

--- nvim/lua/lsp/packages.lua
+++ nvim/lua/lsp/packages.lua
@@ -10,7 +10,25 @@ packer.use({
 
 -- config object
 
-packer.use("neovim/nvim-lspconfig")
+packer.use({
+	"neovim/nvim-lspconfig",
+	config = function()
+		-- Language configs
+		require("languages.cpp")
+		require("languages.docker")
+		require("languages.go")
+		require("languages.julia")
+		require("languages.kotlin")
+		require("languages.lua")
+		require("languages.misc")
+		require("languages.python")
+		require("languages.r")
+		require("languages.rust")
+		require("languages.scala")
+		require("languages.vim")
+		require("languages.yaml")
+	end,
+})
 packer.use("nvim-lua/lsp_extensions.nvim")
 packer.use("nvim-lua/completion-nvim")
 packer.use("simrat39/rust-tools.nvim") -- LSP server extensions for Rust
