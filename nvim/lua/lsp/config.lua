7c09aba4990183ea46f9668058752286a7a2368f
Jan Steinke <jan.steinke@gmail.com>
2021-09-29 16:22:25 +0200

    telescope config

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -13,10 +13,16 @@ function M.on_attach(client, bufnr)
 	print("client:" .. client.name .. "; bufnr:" .. bufnr)
 	-- print(vim.inspect(client.resolved_capabilities))
 
+	-- Workspace diagnostics
 	local normal_mode_keymap = {
-		d = { "<cmd>Telescope lsp_workspace_diagnostics<CR>", "Diagnostics", noremap = true },
+		d = {
+			"<cmd>lua require'telescope.builtin'.lsp_workspace_diagnostics(require('telescope.themes').get_ivy({}))<cr>",
+			"Diagnostics",
+			noremap = true,
+		},
 	}
 	local visual_mode_keymap = {}
+	-- Code Lens
 	if client.resolved_capabilities.code_lens then
 		vim.cmd([[autocmd CursorHold,CursorHoldI,InsertLeave <buffer> lua vim.lsp.codelens.refresh()]])
 		normal_mode_keymap["c"] = {
@@ -25,16 +31,27 @@ function M.on_attach(client, bufnr)
 			noremap = true,
 		}
 	end
-
+	-- Format document
 	if client.resolved_capabilities.document_formatting then
 		vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 500)]])
 	end
+	-- Goto Defintion
 	if client.resolved_capabilities.goto_definition then
 		wk.register({ d = { "<cmd>Telescope lsp_definitions<CR>", "Definition", noremap = true } }, { prefix = "g" })
 	end
+	-- Find References
 	if client.resolved_capabilities.find_references then
-		wk.register({ r = { "<cmd>Telescope lsp_references<CR>", "References", noremap = true } }, { prefix = "g" })
+		wk.register({
+			r = {
+				"<cmd>lua require'telescope.builtin'.lsp_references(require('telescope.themes').get_ivy({}))<cr>",
+				"References",
+				noremap = true,
+			},
+		}, {
+			prefix = "g",
+		})
 	end
+	-- Hover
 	if client.resolved_capabilities.hover then
 		normal_mode_keymap["h"] = {
 			"<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>",
@@ -42,6 +59,7 @@ function M.on_attach(client, bufnr)
 			noremap = true,
 		}
 	end
+	-- Rename
 	if client.resolved_capabilities.rename then
 		normal_mode_keymap["r"] = {
 			"<cmd>lua require('lspsaga.rename').rename()<CR>",
@@ -49,6 +67,7 @@ function M.on_attach(client, bufnr)
 			noremap = true,
 		}
 	end
+	-- Code Action
 	if client.resolved_capabilities.code_action then
 		visual_mode_keymap["a"] = {
 			":'<,'>lua require('lspsaga.codeaction').range_code_action()<CR>",
@@ -61,6 +80,7 @@ function M.on_attach(client, bufnr)
 			noremap = true,
 		}
 	end
+	-- Symbols
 	if client.resolved_capabilities.document_symbol then
 		normal_mode_keymap["s"] = {
 			"<cmd>Vista!!<CR>",
