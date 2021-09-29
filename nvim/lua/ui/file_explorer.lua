2a6aefaaaea201b970353b2b87cae687d7e1973d
Jan Steinke <jan.steinke@gmail.com>
2021-09-29 16:28:40 +0200

    new nvim tree config

--- nvim/lua/ui/file_explorer.lua
+++ nvim/lua/ui/file_explorer.lua
@@ -3,14 +3,16 @@ packer.use({
 	"kyazdani42/nvim-tree.lua",
 	requires = { "kyazdani42/nvim-web-devicons", { "folke/which-key.nvim" } },
 	config = function()
-		vim.g.nvim_tree_auto_open = 1
-		vim.g.nvim_tree_auto_close = 0
-		vim.g.nvim_tree_quit_on_open = 0
-		vim.g.nvim_tree_add_trailing = 1
-		vim.g.nvim_tree_lsp_diagnostics = 1
+		require("nvim-tree").setup({
+			auto_open = 1,
+			auto_close = 0,
+			quit_on_open = 0,
+			add_trailing = 1,
+			lsp_diagnostics = 1,
 
-		vim.g.nvim_tree_disable_netrw = 0
-		vim.g.nvim_tree_hijack_netrw = 0
+			disable_netrw = 0,
+			hijack_netrw = 0,
+		})
 
 		local wk = require("which-key")
 		wk.register({
