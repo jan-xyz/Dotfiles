b2b161e774e96923b192ad9fa81b96ccd20bcc5a
Jan Steinke <jan.steinke@gmail.com>
2021-09-30 09:12:28 +0200

    switch to a neovim HEAD compatiple lspsaga

--- nvim/lua/lsp/packages.lua
+++ nvim/lua/lsp/packages.lua
@@ -33,4 +33,4 @@ packer.use("nvim-lua/lsp_extensions.nvim")
 packer.use("nvim-lua/completion-nvim")
 packer.use("simrat39/rust-tools.nvim") -- LSP server extensions for Rust
 packer.use("folke/lua-dev.nvim") -- LSP server extensions for Lua
-packer.use("glepnir/lspsaga.nvim")
+packer.use("tami5/lspsaga.nvim")
