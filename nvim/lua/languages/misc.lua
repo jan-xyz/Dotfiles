ee41c5eeed3bf7f70739ef948d1e299a3bed9efb
Jan Steinke <jan.steinke@gmail.com>
2021-10-08 20:51:00 +0200

    format lua

--- nvim/lua/languages/misc.lua
+++ nvim/lua/languages/misc.lua
@@ -17,7 +17,11 @@ nvim_lsp.efm.setup({
 				{
 					lintCommand = "shellcheck -f gcc -x",
 					lintSource = "shellcheck",
-					lintFormats = { "%f:%l:%c: %trror: %m", "%f:%l:%c: %tarning: %m", "%f:%l:%c: %tote: %m" },
+					lintFormats = {
+						"%f:%l:%c: %trror: %m",
+						"%f:%l:%c: %tarning: %m",
+						"%f:%l:%c: %tote: %m",
+					},
 				},
 			},
 			lua = {
