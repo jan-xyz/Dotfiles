c2c59f1c0a4233bf77c8af456f2a46cb4c894df7
Jan Steinke <jan.steinke@gmail.com>
2022-01-02 21:22:10 +0100

    ToggleTerm

--- nvim/lua/ui/terminal.lua
+++ nvim/lua/ui/terminal.lua
@@ -1,19 +1,23 @@
 local packer = require("packer")
 packer.use({
-	"voldikss/vim-floaterm",
+	"akinsho/toggleterm.nvim",
 	config = function()
 		local wk = require("which-key")
 
+		require("toggleterm").setup({
+			direction = "float",
+		})
+
 		-- keymaps in normal mode
 		wk.register({
-			t = { "<cmd>FloatermToggle<CR>", "Toggle terminal", noremap = true },
+			t = { "<cmd>ToggleTerm<CR>", "Toggle terminal", noremap = true },
 		}, {
 			prefix = "<leader>",
 		})
 
 		-- keymaps in terminal mode
 		wk.register({
-			t = { "<cmd>FloatermToggle<CR>", "Toggle terminal", noremap = true },
+			t = { "<cmd>ToggleTerm<CR>", "Toggle terminal", noremap = true },
 		}, {
 			mode = "t",
 			prefix = "<leader>",
