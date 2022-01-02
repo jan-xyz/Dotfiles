da6afb4df9e51ec14dcedf5ab08e195aa052fdc0
Jan Steinke <jan.steinke@gmail.com>
2022-01-02 22:06:53 +0100

    configure neovim terminal for git

--- nvim/lua/ui/terminal.lua
+++ nvim/lua/ui/terminal.lua
@@ -2,6 +2,10 @@ local packer = require("packer")
 packer.use({
 	"akinsho/toggleterm.nvim",
 	config = function()
+		-- configure neovim nesting with neovim-remote
+		vim.env.GIT_EDITOR = "nvr -cc split --remote-wait"
+		vim.api.nvim_exec([[ autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete ]], false)
+
 		local wk = require("which-key")
 
 		require("toggleterm").setup({
