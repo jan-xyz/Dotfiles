6b25f899d639c107adc0027ba683f0e7504f7123
Jan Steinke <jan.steinke@gmail.com>
2022-01-23 00:25:30 +0100

    switch to hardline implementation

--- nvim/lua/ui/status_line.lua
+++ nvim/lua/ui/status_line.lua
@@ -3,22 +3,9 @@ local packer = require("packer")
 packer.use({
 	"ojroques/nvim-hardline",
 	config = function()
-		local function get_lsp_clients()
-			local clients = vim.lsp.buf_get_clients()
-			if next(clients) == nil then
-				return ""
-			end
-
-			local c = {}
-			for _, client in ipairs(clients) do
-				table.insert(c, client.name)
-			end
-			return table.concat(c, "|")
-		end
-
 		require("hardline").setup({
 			theme = "nord",
-			sections = { -- define sections
+			sections = {
 				{ class = "mode", item = require("hardline.parts.mode").get_item },
 				{ class = "high", item = require("hardline.parts.git").get_item, hide = 100 },
 				{ class = "med", item = require("hardline.parts.filename").get_item },
@@ -29,7 +16,7 @@ packer.use({
 				{ class = "warning", item = require("hardline.parts.lsp").get_warning },
 				{ class = "warning", item = require("hardline.parts.whitespace").get_item },
 				{ class = "high", item = require("hardline.parts.filetype").get_item, hide = 80 },
-				{ class = "high", item = get_lsp_clients, hide = 80 },
+				{ class = "high", item = require("hardline.parts.lsp").get_lsp_clients, hide = 80 },
 				{ class = "mode", item = require("hardline.parts.line").get_item },
 			},
 		})
