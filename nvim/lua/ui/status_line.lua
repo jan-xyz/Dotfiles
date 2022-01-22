3a580b974a3c75d546b59f4da0a9eae70e95570c
Jan Steinke <jan.steinke@gmail.com>
2022-01-22 23:04:38 +0100

    display LSP names in statusline

--- nvim/lua/ui/status_line.lua
+++ nvim/lua/ui/status_line.lua
@@ -3,6 +3,19 @@ local packer = require("packer")
 packer.use({
 	"ojroques/nvim-hardline",
 	config = function()
+		local function get_lsp_clients()
+			local clients = vim.lsp.buf_get_clients()
+			if next(clients) == nil then
+				return ""
+			end
+
+			local c = {}
+			for _, client in ipairs(clients) do
+				table.insert(c, client.name)
+			end
+			return table.concat(c, "|")
+		end
+
 		require("hardline").setup({
 			theme = "nord",
 			sections = { -- define sections
@@ -16,6 +29,7 @@ packer.use({
 				{ class = "warning", item = require("hardline.parts.lsp").get_warning },
 				{ class = "warning", item = require("hardline.parts.whitespace").get_item },
 				{ class = "high", item = require("hardline.parts.filetype").get_item, hide = 80 },
+				{ class = "high", item = get_lsp_clients, hide = 80 },
 				{ class = "mode", item = require("hardline.parts.line").get_item },
 			},
 		})
