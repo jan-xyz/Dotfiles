ebc31e0c2fe4f07f5c3df1bb76209627a1064f87
Jan Steinke <jan.steinke@gmail.com>
2022-02-19 10:51:43 +0100

    don't mess up window layout on close

--- nvim/lua/ui/buffer_line.lua
+++ nvim/lua/ui/buffer_line.lua
@@ -1,12 +1,14 @@
 local packer = require("packer")
 packer.use({
 	"akinsho/nvim-bufferline.lua",
-	requires = "kyazdani42/nvim-web-devicons",
+	requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" },
 	config = function()
 		require("bufferline").setup({
 			options = {
 				diagnostics = "nvim_lsp",
 				offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left" } },
+				close_command = "Bdelete! %d",
+				right_mouse_command = "Bdelete! %d",
 			},
 		})
 	end,
