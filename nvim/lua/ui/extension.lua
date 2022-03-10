5e6b7dbe869874019b8787b56cf549b0d3adbd31
Jan Steinke <jan.steinke@gmail.com>
2022-03-10 18:06:42 +0100

    replace vim.notify

--- nvim/lua/ui/extension.lua
+++ nvim/lua/ui/extension.lua
@@ -1,3 +1,10 @@
 local packer = require("packer")
 
 packer.use({ "stevearc/dressing.nvim" })
+
+packer.use({
+	"rcarriga/nvim-notify",
+	config = function()
+		vim.notify = require("notify")
+	end,
+})
