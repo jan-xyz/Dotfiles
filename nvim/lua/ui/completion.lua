99643e77d6a55c33cb25ce03d3e2b7fc8719054e
Jan Steinke <jan.steinke@gmail.com>
2022-03-10 19:31:10 +0100

    add completion to search and comands

--- nvim/lua/ui/completion.lua
+++ nvim/lua/ui/completion.lua
@@ -66,6 +66,20 @@ packer.use({
 				-- end
 			},
 		})
+
+		cmp.setup.cmdline("/", {
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		cmp.setup.cmdline(":", {
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
 	end,
 })
 
