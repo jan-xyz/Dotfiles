85c300550f3f614025e0de520ef5b95d43a3bcbe
Jan Steinke <jan.steinke@gmail.com>
2022-04-10 15:19:29 +0200

    set loglevel for vim-notify

--- nvim/lua/ui/extension.lua
+++ nvim/lua/ui/extension.lua
@@ -5,6 +5,7 @@ packer.use({ "stevearc/dressing.nvim" })
 packer.use({
 	"rcarriga/nvim-notify",
 	config = function()
+		require("notify").setup({ level = "info" })
 		vim.notify = require("notify")
 	end,
 })
