8c77a266d5517c52f18981412dc7412f91025c07
Jan Steinke <jan.steinke@gmail.com>
2022-04-10 17:45:44 +0200

    reduce log level for on_attach

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -4,7 +4,7 @@ local M = {}
 
 -- Global callback functions for LSP shortcuts
 function M.on_attach(client, bufnr)
-	vim.notify("connecting '" .. client.name .. "' to buffer " .. bufnr, vim.log.levels.INFO)
+	vim.notify("connecting '" .. client.name .. "' to buffer " .. bufnr, vim.log.levels.DEBUG)
 	-- vim.notify(vim.inspect(client.resolved_capabilities), vim.log.levels.DEBUG)
 
 	local wk = require("which-key")
