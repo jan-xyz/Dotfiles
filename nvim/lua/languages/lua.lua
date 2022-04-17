9b3bcc7bbd4026f0a1b01ae48319700627586d7e
Jan Steinke <jan.steinke@gmail.com>
2022-04-17 09:49:40 +0200

    configure lua formatter

--- nvim/lua/languages/lua.lua
+++ nvim/lua/languages/lua.lua
@@ -6,13 +6,32 @@ packer.use({
 		local nvim_lsp = require("lspconfig")
 		local lsp = require("lsp.config")
 		local completion = require("ui.completion")
-		local luadev = require("lua-dev").setup({
+		local opts = {
+			settings = {
+				Lua = {
+					diagnostics = {
+						neededFileStatus = {
+							["codestyle-check"] = "Any",
+						},
+					},
+					format = {
+						enable = true,
+						defaultConfig = {
+							quote_style = "double",
+							indent_style = "tab",
+							indent_size = "1",
+						},
+					},
+				},
+			},
+		}
+		opts = vim.tbl_deep_extend("force", require("lua-dev").setup({
 			lspconfig = {
 				capabilities = completion.capabilities,
 				on_attach = lsp.on_attach,
 				cmd = { "lua-language-server" },
 			},
-		})
-		nvim_lsp.sumneko_lua.setup(luadev)
+		}), opts)
+		nvim_lsp.sumneko_lua.setup(opts)
 	end,
 }) -- LSP server extensions for Lua
