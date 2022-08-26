62556b6dd507ad2191deb77a04289e1d18d2fda6
Jan Steinke <jan.steinke@gmail.com>
2022-08-26 22:59:52 +0200

    remove global rootcommand

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -13,26 +13,14 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "dotfiles",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+// Execute the dotfile application
 func Execute() {
 	cobra.OnInitialize(initConfig)
 
+	rootCmd := &cobra.Command{
+		Use:   "dotfiles",
+		Short: "manage your dotfiles",
+	}
 	rootCmd.AddCommand(updateCmd)
 	rootCmd.AddCommand(installCmd)
 
