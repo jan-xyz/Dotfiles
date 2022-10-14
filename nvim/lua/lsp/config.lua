20329f430cb0ae1c5d012e7b37d3d77d78c67d2b
Jan Steinke <jan.steinke@moia.io>
2022-10-14 21:18:17 +0200

    remove deprecated function

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -46,7 +46,7 @@ function M.on_attach(client, bufnr)
 	if client.server_capabilities.documentFormattingProvider then
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			callback = function()
-				vim.lsp.buf.formatting_sync({}, 500)
+				vim.lsp.buf.format()
 			end,
 			buffer = 0,
 		})
