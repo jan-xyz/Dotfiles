6f76dbee9076e36813c3be195cee81d2e94eab46
Jan Steinke <jan.steinke@gmail.com>
2022-10-31 17:21:48 +0100

    more tests

--- internal/plugins/golang/plugin_test.go
+++ internal/plugins/golang/plugin_test.go
@@ -8,57 +8,170 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetMissingGoModules(t *testing.T) {
-	commander := dotfiles.MockCommander{}
-	defer commander.AssertExpectations(t)
-	commander.OnOutput("command", []string{"-v", "bar"}).Return(nil, nil)
-	commander.OnOutput("command", []string{"-v", "foo"}).Return(nil, errors.New("some-error"))
-	b := Plugin{
-		Modules: []GoModule{
-			{Exe: "bar"},
-			{Exe: "foo"},
+func TestGetMissingPackage(t *testing.T) {
+	type prepareCommander = func(c *dotfiles.MockCommander)
+	tests := []struct {
+		name               string
+		configuredPackages []GoModule
+		prepareCommander   prepareCommander
+		expectedPackages   []string
+		wantErr            bool
+	}{
+		{
+			name: "succesfully get packages",
+			configuredPackages: []GoModule{
+				{Exe: "bar"},
+				{Exe: "foo"},
+			},
+			prepareCommander: func(c *dotfiles.MockCommander) {
+				c.OnOutput("command", []string{"-v", "bar"}).Return(nil, nil)
+				c.OnOutput("command", []string{"-v", "foo"}).Return(nil, errors.New("some-error"))
+			},
+			expectedPackages: []string{"foo"},
+			wantErr:          false,
 		},
-		Commander: commander.Output,
 	}
-	missingPackages, err := b.GetMissingPackages()
-	assert.NoError(t, err)
-	assert.Equal(t, []string{"foo"}, missingPackages)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			commander := dotfiles.MockCommander{}
+			defer commander.AssertExpectations(t)
+			tt.prepareCommander(&commander)
+
+			b := Plugin{
+				Modules:   tt.configuredPackages,
+				Commander: commander.Output,
+			}
+			actualLinks, err := b.GetMissingPackages()
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.ElementsMatch(t, tt.expectedPackages, actualLinks)
+			}
+		})
+	}
 }
 
-func TestInstallingGoModules(t *testing.T) {
-	commander := dotfiles.MockCommander{}
-	defer commander.AssertExpectations(t)
-	commander.OnOutput("go", []string{"install", "github.com/bar@latest"}).Return(nil, nil)
-	commander.OnOutput("go", []string{"install", "github.com/foo@latest"}).Return(nil, nil)
-	b := Plugin{
-		Modules: []GoModule{
-			{Exe: "bar", Path: "github.com/bar"},
-			{Exe: "foo", Path: "github.com/foo"},
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		name               string
+		input              []string
+		configuredPackages []GoModule
+		prepareCommander   func(c *dotfiles.MockCommander)
+		wantErr            bool
+	}{
+		{
+			name:  "succefully install packages",
+			input: []string{"bar", "foo"},
+			configuredPackages: []GoModule{
+				{Exe: "bar", Path: "github.com/bar"},
+				{Exe: "foo", Path: "github.com/foo"},
+			},
+			prepareCommander: func(c *dotfiles.MockCommander) {
+				c.OnOutput("go", []string{"install", "github.com/bar@latest"}).Return(nil, nil)
+				c.OnOutput("go", []string{"install", "github.com/foo@latest"}).Return(nil, nil)
+			},
+			wantErr: false,
 		},
-		Commander: commander.Output,
+		{
+			name:  "ignore empty input",
+			input: []string{},
+			prepareCommander: func(c *dotfiles.MockCommander) {
+			},
+			wantErr: false,
+		},
+		{
+			name:  "ignore not configured binaries",
+			input: []string{"foo"},
+			prepareCommander: func(c *dotfiles.MockCommander) {
+			},
+			wantErr: false,
+		},
+		{
+			name:  "fails if calling python fails",
+			input: []string{"bar", "foo"},
+			configuredPackages: []GoModule{
+				{Exe: "bar", Path: "github.com/bar"},
+				{Exe: "foo", Path: "github.com/foo"},
+			},
+			prepareCommander: func(c *dotfiles.MockCommander) {
+				c.OnOutput("go", []string{"install", "github.com/bar@latest"}).Return(nil, nil)
+				c.OnOutput("go", []string{"install", "github.com/foo@latest"}).Return(nil, errors.New("boom"))
+			},
+			wantErr: true,
+		},
 	}
-	err := b.Add([]string{"bar", "foo"})
-	assert.NoError(t, err)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			commander := dotfiles.MockCommander{}
+			defer commander.AssertExpectations(t)
+			tt.prepareCommander(&commander)
+
+			b := Plugin{
+				Modules:   tt.configuredPackages,
+				Commander: commander.Output,
+			}
+			err := b.Add(tt.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
 
-func TestTryingToInstallGoModulesWithEmptyListDoesNotCallBrew(t *testing.T) {
-	b := Plugin{}
-	err := b.Add([]string{})
-	assert.NoError(t, err)
-}
-
-func TestUpdatingGoModules(t *testing.T) {
-	commander := dotfiles.MockCommander{}
-	defer commander.AssertExpectations(t)
-	commander.OnOutput("go", []string{"install", "github.com/bar@latest"}).Return(nil, nil)
-	commander.OnOutput("go", []string{"install", "github.com/foo@latest"}).Return(nil, nil)
-	b := Plugin{
-		Modules: []GoModule{
-			{Exe: "bar", Path: "github.com/bar"},
-			{Exe: "foo", Path: "github.com/foo"},
+func TestUpdate(t *testing.T) {
+	tests := []struct {
+		name               string
+		configuredPackages []GoModule
+		prepareCommander   func(c *dotfiles.MockCommander)
+		wantErr            bool
+	}{
+		{
+			name: "succesfully update packages",
+			configuredPackages: []GoModule{
+				{Exe: "bar", Path: "github.com/bar"},
+				{Exe: "foo", Path: "github.com/foo"},
+			},
+			prepareCommander: func(c *dotfiles.MockCommander) {
+				c.OnOutput("go", []string{"install", "github.com/bar@latest"}).Return(nil, nil)
+				c.OnOutput("go", []string{"install", "github.com/foo@latest"}).Return(nil, nil)
+			},
+			wantErr: false,
 		},
-		Commander: commander.Output,
+		{
+			name: "fail update it python fails",
+			configuredPackages: []GoModule{
+				{Exe: "bar", Path: "github.com/bar"},
+				{Exe: "foo", Path: "github.com/foo"},
+			},
+			prepareCommander: func(c *dotfiles.MockCommander) {
+				c.OnOutput("go", []string{"install", "github.com/bar@latest"}).Return(nil, nil)
+				c.OnOutput("go", []string{"install", "github.com/foo@latest"}).Return(nil, errors.New("boom"))
+			},
+			wantErr: true,
+		},
 	}
-	err := b.Update()
-	assert.NoError(t, err)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			commander := dotfiles.MockCommander{}
+			defer commander.AssertExpectations(t)
+			tt.prepareCommander(&commander)
+
+			b := Plugin{
+				Modules:   tt.configuredPackages,
+				Commander: commander.Output,
+			}
+			err := b.Update()
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
