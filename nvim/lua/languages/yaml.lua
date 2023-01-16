15fbc866ba62be7593025c7c0d498f083f6a463a
Jan Steinke <jan.steinke@moia.io>
2023-01-16 09:44:34 +0100

    add serverless yaml to yamlls

--- nvim/lua/languages/yaml.lua
+++ nvim/lua/languages/yaml.lua
@@ -15,6 +15,7 @@ nvim_lsp.yamlls.setup({
 				["https://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
 				["https://json.schemastore.org/github-action"] = ".github/**/action.{yml,yaml}",
 				["https://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+				["https://raw.githubusercontent.com/lalcebo/json-schema/master/serverless/reference.json"] = "serverless.{yml,yaml}",
 				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.22.2-standalone-strict/all.json"] = "**/*.k8s.{yml,yaml}",
 			},
 			customTags = {
