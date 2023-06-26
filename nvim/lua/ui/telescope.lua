9341595f4f7704766402e4cdd679f0d2b90dd71c
Jan Steinke <jan.steinke@gmail.com>
2023-06-26 22:04:36 +0200

    add go test alternation

--- nvim/lua/ui/telescope.lua
+++ nvim/lua/ui/telescope.lua
@@ -6,7 +6,10 @@ packer.use({
 		"nvim-lua/plenary.nvim",
 		"folke/which-key.nvim",
 		"nvim-lua/plenary.nvim",
+
+		-- Plugins
 		"nvim-telescope/telescope-file-browser.nvim",
+		"otavioschwanck/telescope-alternate",
 	},
 	config = function()
 		require("telescope").setup({
@@ -43,9 +46,17 @@ packer.use({
 					git_status = true,
 					hidden = true,
 				},
+				["telescope-alternate"] = {
+					mappings = {
+						{ "(.*).go",      { { "[1]_test.go", "Test", true } } },
+						{ "(.*)_test.go", { { "[1].go", "Original", true } } },
+					},
+				}
 			},
 		})
+
 		require("telescope").load_extension("file_browser")
+		require("telescope").load_extension("telescope-alternate")
 
 		local wk = require("which-key")
 		local telescope_builtin = require("telescope.builtin")
@@ -61,6 +72,11 @@ packer.use({
 		local find_file = function()
 			telescope_builtin.find_files({ hidden = true })
 		end
+
+		local alternate = function()
+			require("telescope-alternate.telescope").alternate({})
+		end
+
 		wk.register({
 			f = {
 				name = "File", -- optional group name
@@ -83,6 +99,7 @@ packer.use({
 		wk.register({
 			e = { file_browser, "File explorer", noremap = true },
 			f = { current_file, "Current file in file explorer", noremap = true },
+			t = { alternate, "Alternate Test/File", noremap = true },
 		}, {
 			prefix = "f",
 		})
