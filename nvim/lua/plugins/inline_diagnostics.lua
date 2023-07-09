884ba0664831f8a5672dd8a0823cf2a6a627ffb2
Jan Steinke <jan.steinke@gmail.com>
2023-07-09 02:24:04 +0200

    remove weird toggle

--- nvim/lua/plugins/inline_diagnostics.lua
+++ nvim/lua/plugins/inline_diagnostics.lua
@@ -1,18 +1,12 @@
--- Disable the plugin in Lazy.nvim
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "lazy",
-	callback = function()
-		local previous = not require("lsp_lines").toggle()
-		if not previous then
-			require("lsp_lines").toggle()
-		end
-	end,
-})
 return {
 	{
 		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-		opts = {
-			virtual_text = false,
-		},
-	},
+		config = function()
+			require("lsp_lines").setup()
+			-- Disable virtual_text since it's redundant due to lsp_lines.
+			vim.diagnostic.config({
+				virtual_text = false,
+			})
+		end,
+	}
 }
