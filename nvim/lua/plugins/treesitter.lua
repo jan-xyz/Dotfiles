feb991eb432b071a77e927242b14a65ff0c65f4c
Jan Steinke <jan.steinke@gmail.com>
2023-07-09 13:33:38 +0200

    fix tree-sitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,28 +2,25 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = "all",
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
+		dependencies = {
+			"nvim-treesitter/playground",
 		},
-	},
-	{
-		"nvim-treesitter/playground",
 		config = function()
-			require("nvim-treesitter.configs").setup({})
-		end,
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = "all",
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				}
+			})
+		end
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			require("treesitter-context").setup({
-				enbaled = true,
-				mode = "topline",
-			})
-		end,
+		opts = {
+			enbaled = true,
+			mode = "topline",
+		}
 	},
 }
