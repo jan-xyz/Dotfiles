88862c04e404635f7edaa255831c361b2b64d4dc
Jan Steinke <jan.steinke@gmail.com>
2023-10-03 12:18:00 +0200

    fix linter issues

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,21 +1,22 @@
-return { {
-	"mhartington/formatter.nvim",
-	config = function()
-		-- Utilities for creating configurations
-		require("formatter").setup({
-			filetype = {
-				proto = { { exe = "buf", args = { "format" }, stdin = true } },
-				sh = { require("formatter.filetypes.sh").shfmt },
-				markdown = { { exe = "markdownlint", args = { "--fix" }, ignore_exitcode = true } },
-			}
+return {
+	{
+		"mhartington/formatter.nvim",
+		config = function()
+			-- Utilities for creating configurations
+			require("formatter").setup({
+				filetype = {
+					proto = { { exe = "buf", args = { "format" }, stdin = true } },
+					sh = { require("formatter.filetypes.sh").shfmt },
+					markdown = { { exe = "markdownlint", args = { "--fix" }, ignore_exitcode = true } },
+				},
+			})
 
-		})
-
-		-- autoformat on save
-		vim.api.nvim_create_autocmd("BufWritePost", {
-			callback = function()
-				vim.cmd("FormatWrite")
-			end,
-		})
-	end
-} }
+			-- autoformat on save
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				callback = function()
+					vim.cmd("FormatWrite")
+				end,
+			})
+		end,
+	},
+}
