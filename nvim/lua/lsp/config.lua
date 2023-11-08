87588919ac25f82cf367066b67e216604bbc0d45
Jan Steinke <jan.steinke@gmail.com>
2023-11-08 12:56:14 +0100

    set local buffer when running on_attach

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -17,12 +17,10 @@ function M.on_attach(client, bufnr)
 	local current_file_diagnostics = function()
 		telescope_builtin.diagnostics({ bufnr = 0 })
 	end
-	vim.keymap.set("n", "<leader>d", current_file_diagnostics, { noremap = true, desc = "Open diagnostics picker" })
-	vim.keymap.set(
-		"n",
-		"<leader>D",
-		telescope_builtin.diagnostics,
-		{ noremap = true, desc = "Open workspace diagnostics picker" }
+	vim.keymap.set("n", "<leader>d", current_file_diagnostics,
+		{ noremap = true, buffer = bufnr, desc = "Open diagnostics picker" })
+	vim.keymap.set("n", "<leader>D", telescope_builtin.diagnostics,
+		{ noremap = true, buffer = bufnr, desc = "Open workspace diagnostics picker" }
 	)
 
 	-- Code Lens
@@ -31,7 +29,8 @@ function M.on_attach(client, bufnr)
 			{ "BufEnter", "CursorHold", "InsertLeave" },
 			{ callback = vim.lsp.codelens.refresh, buffer = bufnr }
 		)
-		vim.keymap.set("n", "<leader>c", vim.lsp.codelens.run, { noremap = true, desc = "Peform codelens" })
+		vim.keymap.set("n", "<leader>c", vim.lsp.codelens.run,
+			{ noremap = true, buffer = bufnr, desc = "Peform codelens" })
 	end
 
 	-- Format document
@@ -39,68 +38,57 @@ function M.on_attach(client, bufnr)
 		local format = function()
 			vim.lsp.buf.format({ async = false })
 		end
-		vim.api.nvim_create_autocmd("BufWritePre", { callback = format, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre",
+			{ callback = format, buffer = bufnr })
 	end
 
 	-- Goto Defintion
 	if client.server_capabilities.definitionProvider then
-		vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions, { noremap = true, desc = "Goto definition" })
+		vim.keymap.set("n", "gd", telescope_builtin.lsp_definitions,
+			{ noremap = true, buffer = bufnr, desc = "Goto definition" })
 	end
 	-- Goto Implementations
 	if client.server_capabilities.implementationProvider then
-		vim.keymap.set(
-			"n",
-			"gi",
-			telescope_builtin.lsp_implementations,
-			{ noremap = true, desc = "Goto Implementations" }
+		vim.keymap.set("n", "gi", telescope_builtin.lsp_implementations,
+			{ noremap = true, buffer = bufnr, desc = "Goto Implementations" }
 		)
 	end
 
 	-- Find References
 	if client.server_capabilities.referencesProvider then
-		vim.keymap.set("n", "gr", telescope_builtin.lsp_references, { noremap = true, desc = "Goto references" })
+		vim.keymap.set("n", "gr", telescope_builtin.lsp_references,
+			{ noremap = true, buffer = bufnr, desc = "Goto references" })
 	end
 
 	-- Hover
 	if client.server_capabilities.hoverProvider then
-		vim.keymap.set(
-			"n",
-			"<leader>k",
-			vim.lsp.buf.hover,
-			{ noremap = true, desc = "Show docs for item under cursor" }
+		vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover,
+			{ noremap = true, buffer = bufnr, desc = "Show docs for item under cursor" }
 		)
 	end
 
 	-- Rename
 	if client.server_capabilities.renameProvider then
-		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { noremap = true, desc = "Rename symbol" })
+		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename,
+			{ noremap = true, buffer = bufnr, desc = "Rename symbol" })
 	end
 
 	-- Code Action
 	if client.server_capabilities.codeActionProvider then
-		vim.keymap.set(
-			{ "n", "v" },
-			"<leader>a",
-			vim.lsp.buf.code_action,
-			{ noremap = true, desc = "Perform code action" }
+		vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action,
+			{ noremap = true, buffer = bufnr, desc = "Perform code action" }
 		)
 	end
 
 	-- Symbols
 	if client.server_capabilities.workspaceSymbolProvider then
-		vim.keymap.set(
-			"n",
-			"<leader>S",
-			telescope_builtin.lsp_dynamic_workspace_symbols,
-			{ noremap = true, desc = "Open workspace symbol picker" }
+		vim.keymap.set("n", "<leader>S", telescope_builtin.lsp_dynamic_workspace_symbols,
+			{ noremap = true, buffer = bufnr, desc = "Open workspace symbol picker" }
 		)
 	end
 	if client.server_capabilities.documentSymbolProvider then
-		vim.keymap.set(
-			"n",
-			"<leader>s",
-			telescope_builtin.lsp_document_symbols,
-			{ noremap = true, desc = "Open symbol picker" }
+		vim.keymap.set("n", "<leader>s", telescope_builtin.lsp_document_symbols,
+			{ noremap = true, buffer = bufnr, desc = "Open symbol picker" }
 		)
 	end
 end
