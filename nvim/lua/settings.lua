13d656959a166fc17ef84f361d0f292412ecc421
Jan Steinke <jan.steinke@gmail.com>
2023-11-11 11:48:34 +0100

    stylua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -39,7 +39,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Hightlight selection on yank",
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank { higroup = "IncSearch", timeout = 300 }
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
 	end,
 })
 
