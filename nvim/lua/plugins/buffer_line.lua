bdf4ae6b4c010933865d4dde594cd469fe48fbe4
Jan Steinke <jan.steinke@gmail.com>
2023-11-11 17:58:30 +0100

    sync and replace bufferline

--- nvim/lua/plugins/buffer_line.lua
+++ nvim/lua/plugins/buffer_line.lua
@@ -1,9 +1,15 @@
 return {
+
 	{
-		"romgrk/barbar.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			animation = false,
-		},
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					diagnostics = "nvim_lsp",
+				},
+			})
+		end,
 	},
 }
