f4a58f18241f068f35676ef10c06e05bf07292f9
Jan Steinke <jan.steinke@gmail.com>
2023-11-13 18:13:33 +0100

    upgrade to fixed wf.nvim version for leader substitution

--- nvim/lua/plugins/keymapprev.lua
+++ nvim/lua/plugins/keymapprev.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"Cassin01/wf.nvim",
-		version = "*",
 		config = function()
 			-- Which Key
 			local groups = {
@@ -13,7 +12,7 @@ return {
 			vim.keymap.set(
 				"n",
 				"<Leader>",
-				which_key({ text_insert_in_advance = "<Space>", key_group_dict = groups }),
+				which_key({ text_insert_in_advance = "<Leader>", key_group_dict = groups }),
 				{ noremap = true, silent = true, desc = "/" }
 			)
 			vim.keymap.set(
