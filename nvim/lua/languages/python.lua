87fe2e09bf1f862501a8de2798905a6220b51259
Jan Steinke <jan.steinke@gmail.com>
2023-11-21 22:02:40 +0100

    dynamically determine python environment

--- nvim/lua/languages/python.lua
+++ nvim/lua/languages/python.lua
@@ -5,10 +5,28 @@ function M.setup()
 	local lsp = require("lsp.config")
 	local completion = require("cmp_nvim_lsp")
 
-	nvim_lsp.pylsp.setup({
+	local config = {
 		capabilities = completion.default_capabilities(),
 		on_attach = lsp.on_attach,
-	})
+	}
+
+	-- dynamically determine which interpreter to pick for the project
+	local cwd = vim.loop.cwd()
+	local possible_environments = {
+		cwd .. "/venv/bin/python",
+		cwd .. "/.venv/bin/python",
+		cwd .. "/virtualenv/bin/python",
+	}
+	for _, environment in ipairs(possible_environments) do
+		local f = io.open(environment, "r")
+		if f ~= nil and io.close(f) then
+			vim.notify("setting python environment for lsp to: " .. environment, vim.log.levels.DEBUG)
+			config["settings"] = { pylsp = { plugins = { jedi = { environment = environment } } } }
+			break
+		end
+	end
+
+	nvim_lsp.pylsp.setup(config)
 end
 
 return M
