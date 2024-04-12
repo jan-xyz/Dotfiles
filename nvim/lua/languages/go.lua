4a2d0554480b78ffa0bfc4d14d9420e1e1d3ab87
Jan Steinke <jan.steinke@gmail.com>
2024-04-12 19:52:15 +0200

    Revert "simplify go organize imports"
    This reverts commit b33c53bfb92769548b1b9fc73cf3ca15a6cfa686.

--- nvim/lua/languages/go.lua
+++ nvim/lua/languages/go.lua
@@ -7,10 +7,18 @@ function M.setup()
 	vim.api.nvim_create_autocmd("BufWritePre", {
 		pattern = { "*.go" },
 		callback = function()
-			vim.lsp.buf.code_action({
-				apply = true,
-				context = { only = { "source.organizeImports" } },
-			})
+			local params = vim.lsp.util.make_range_params(nil, "utf-16")
+			params.context = { only = { "source.organizeImports" } }
+			local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+			for _, res in pairs(result or {}) do
+				for _, r in pairs(res.result or {}) do
+					if r.edit then
+						vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+					else
+						vim.lsp.buf.execute_command(r.command)
+					end
+				end
+			end
 		end,
 	})
 
