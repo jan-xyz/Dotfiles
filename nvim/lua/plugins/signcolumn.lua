4877fdfd09013b143ca251e799b2aedf0af288f4
Jan Steinke <jan.steinke@gmail.com>
2024-05-16 21:44:23 +0200

    bump plugins and 0.10 fixes

--- nvim/lua/plugins/signcolumn.lua
+++ nvim/lua/plugins/signcolumn.lua
@@ -1,13 +1,23 @@
 return {
 	{
 		"luukvbaal/statuscol.nvim",
+		branch = "0.10",
 		config = function()
+			-- custom config
 			local builtin = require("statuscol.builtin")
+			local relnumfunc = function(args, fa)
+				args.rnu = true
+				return builtin.lnumfunc(args, fa)
+			end
+
+			-- setup
 			require("statuscol").setup({
-				relculright = true,
 				segments = {
-					{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-					{ text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+					-- fold column
+					{ text = { builtin.foldfunc },                                             click = "v:lua.ScFa" },
+					-- number column
+					{ text = { relnumfunc },                                                   click = "v:lua.ScLa" },
+					-- anything else
 					{ sign = { name = { ".*" }, text = { ".*" }, maxwidth = 2, colwidth = 1 }, click = "v:lua.ScSa" },
 				},
 			})
