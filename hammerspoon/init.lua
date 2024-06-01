bb7a323e75cde9bd8591d59a834e8d502af785e3
Jan Steinke <jan.steinke@gmail.com>
2024-06-02 00:08:42 +0200

    add more configuration

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,8 @@
+-- macro keys:
+--  - layer 1: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+--  - layer 2: a, b, c, d, e, f, g, h, i, j
+--  - layer 3: <not yet set>
+
 -- global mute
 hs.hotkey.bind({ "shift", "ctrl", "alt" }, "0", nil, function()
 	local audio = hs.audiodevice.defaultInputDevice()
@@ -9,13 +14,17 @@ hs.hotkey.bind({ "shift", "ctrl", "alt" }, "0", nil, function()
 			hs.alert.show("muting " .. audio:name())
 		end
 		audio:setInputMuted(not muted)
+	else
+		hs.alert.show("no audio input device")
 	end
 end)
 
 -- focus apps
 local apps = {
-	"Kitty", -- 1
-	"Safari", -- 2
+	"Kitty",
+	"Safari",
+	"Music",
+	"Steam",
 }
 for index, item in ipairs(apps) do
 	local shortcut = tostring(index)
@@ -25,6 +34,19 @@ for index, item in ipairs(apps) do
 	end)
 end
 
+-- audio playback
+hs.hotkey.bind({ "shift", "ctrl", "alt" }, "a", nil, function()
+	hs.itunes.displayCurrentTrack()
+end)
+hs.hotkey.bind({ "shift", "ctrl", "alt" }, "b", nil, function()
+	hs.itunes.previous()
+end)
+hs.hotkey.bind({ "shift", "ctrl", "alt" }, "c", nil, function()
+	hs.itunes.playpause()
+end)
+hs.hotkey.bind({ "shift", "ctrl", "alt" }, "d", nil, function()
+	hs.itunes.next()
+end)
 
 -- Reload the Hammerspoon configuration
 local reloadConfig = function(files)
