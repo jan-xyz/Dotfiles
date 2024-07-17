8b3d49a6c95790126409e7fc6dafa960c1d82a88
Jan Steinke <jan.steinke@gmail.com>
2024-07-17 20:56:54 +0200

    migrate more options to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,45 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+#
+# Homebrew/local
+#
+set --export PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
+set --export PATH /usr/local/bin /usr/local/sbin $PATH
+
+#
+# Pyenv
+#
+set --export PYENV_ROOT $HOME/.pyenv
+set --export PATH $PYENV_ROOT/bin $PATH
+set --export WORKON_HOME $HOME/Envs
+pyenv init - | source
+
 #
 # Go
 #
 set --export GOPATH $HOME/Go
+set --export PATH $PATH $GOPATH/bin
+
+#
+# Rust
+#
+set --export CARGO_NET_GIT_FETCH_WITH_CLI true
+set --export PATH $PATH /usr/local/opt/llvm/bin/
+
+#
+# Scala
+#
+set --export PATH $PATH $HOME/Library/Application\ Support/Coursier/bin
+
+#
+# Editors
+#
+set --export EDITOR '/opt/homebrew/bin/nvim'
+set --export VISUAL '/opt/homebrew/bin/nvim'
+set --export PAGER 'less'
 
 # diable welcome message
 set fish_greeting
+
+#
+# cd
+#
+zoxide init --cmd cd fish | source
--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,3 +1,4 @@
+# https://fishshell.com/docs/current/cmds/fish_title.html
 function fish_title
     # current command
     set -l command (status current-command)
