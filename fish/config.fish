36e6b8778e75bad9d051bd270cbb89fd0f4c326c
Jan Steinke <jan.steinke@gmail.com>
2024-07-20 09:45:21 +0200

    fix fzf colors

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set --export PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
 #
 # fzf
 #
-set --export FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1--color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1--color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac--color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b"
+set --export FZF_DEFAULT_OPTS "--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b"
 
 #
 # Pyenv
