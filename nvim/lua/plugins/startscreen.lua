b06720b3ae01bcf061cbe492f1c78789881725f6
Jan Steinke <jan.steinke@moia.io>
2024-11-11 15:15:55 +0100

    add fortune tips

--- nvim/lua/plugins/startscreen.lua
+++ nvim/lua/plugins/startscreen.lua
@@ -1,7 +1,10 @@
 return {
 	{
 		"nvimdev/dashboard-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			{ "rubiin/fortune.nvim", opts = { content_type = "tips" } },
+		},
 		config = function()
 			require("dashboard").setup({
 				theme = "doom",
@@ -62,6 +65,18 @@ return {
 							key = "u",
 						},
 					},
+					footer = function()
+						local stats = require("lazy").stats()
+						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+						local footer = {
+							"Startuptime: " .. ms .. "ms",
+							"Plugins: " .. stats.loaded .. " loaded / " .. stats.count .. " installed ",
+						}
+						local fortune = require("fortune").get_fortune()
+						footer = vim.list_extend(footer, fortune)
+
+						return footer
+					end,
 				},
 			})
 			vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#A3BE8C", ctermfg = 2 })
