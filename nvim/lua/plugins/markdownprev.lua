18c939bba0476eba2337269138db0796c251c9cc
Jan Steinke <me@janxyz.de>
2025-02-06 09:26:01 +0100

    improve markdownpreview installation

--- nvim/lua/plugins/markdownprev.lua
+++ nvim/lua/plugins/markdownprev.lua
@@ -1,8 +1,10 @@
 return {
 	{
 		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
 		build = function()
+			vim.cmd("Lazy load markdown-preview.nvim")
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
